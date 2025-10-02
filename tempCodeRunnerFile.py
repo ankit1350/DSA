6f241fcaf854b586d39f78b2efc48b464b20bb55
ankit1350 <mrankitak135@gmail.com>
2025-10-02 19:16:47 +0530

    mini project DSA

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,50 +1,17 @@
-class Stack:
+class Node:
     def __init__(self):
-        self.items = []
-
-    def push(self, item):
-        self.items.append(item)
-
-    def pop(self):
-        return self.items.pop()
-
-    def isEmpty(self):
-        return len(self.items) == 0   # ✅ fixed
-
-    def peek(self):
-        return self.items[-1]
-
-    def __str__(self):
-        return str(self.items)
-
-
-def infixToPostfix(infixexpression):
-    prec = {"*": 3, "/": 3, "+": 2, "-": 2, "(": 1}
-    opstack = Stack()
-    postfixList = []
-    tokenList = infixexpression.split()
-
-    for token in tokenList:
-        if token.isalnum():  # ✅ handles numbers and variables
-            postfixList.append(token)
-        elif token == "(":
-            opstack.push(token)
-        elif token == ")":
-            topToken = opstack.pop()
-            while topToken != "(":
-                postfixList.append(topToken)
-                topToken = opstack.pop()
-        else:  # operator
-            while (not opstack.isEmpty()) and (prec[opstack.peek()] >= prec[token]):
-                postfixList.append(opstack.pop())
-            opstack.push(token)
-
-    while not opstack.isEmpty():
-        postfixList.append(opstack.pop())
-
-    return " ".join(postfixList)
-
-
-# Example Run
-infix = input("Enter the infix expression to convert to postfix expression: ")
-print("Postfix Expression:", infixToPostfix(infix))
+        self.data=None
+        self.next=None
+class LinkedList:
+    def __init__(self):
+        self.head=None
+    def length(self):
+        current=self.head
+        if current==None:
+            return
+        count=1
+        current=current.next
+        while current!=self.head:
+            current=current.next
+            count+=1
+        return count
